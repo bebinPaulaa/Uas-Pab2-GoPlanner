dcf358e6a179039ccf2a4e3a61d90cf5623d3828
zaraazzar <azzar.arza18@gmail.com>
2023-05-26 02:11:24 +0700

    add icon images

---
+++ app/src/main/java/com/if4b/goplanner/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.if4b.goplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
