ad10949c03478e474b14fcabcd51625d57fd05a0
zaraazzar <azzar.arza18@gmail.com>
2023-05-26 02:00:25 +0700

    update splashScreen & Manifest

--- app/src/main/java/com/if4b/goplanner/SplashActivity.java
+++ app/src/main/java/com/if4b/goplanner/SplashActivity.java
@@ -2,13 +2,32 @@ package com.if4b.goplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.if4b.goplanner.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding= ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getActionBar().hide();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1500);
+
     }
 }
