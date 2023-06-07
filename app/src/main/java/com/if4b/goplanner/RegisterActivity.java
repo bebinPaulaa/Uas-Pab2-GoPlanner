f817fc5d97a995ef02455660b040eab78696ac30
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-06-07 22:57:16 +0700

    Added activity_register

--- app/src/main/java/com/if4b/goplanner/RegisterActivity.java
+++ app/src/main/java/com/if4b/goplanner/RegisterActivity.java
@@ -2,10 +2,17 @@ package com.if4b.goplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 import com.if4b.goplanner.databinding.ActivityRegisterBinding;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
@@ -23,6 +30,82 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = binding.etUsername.getText().toString();
                 String password = binding.etPassword.getText().toString();
+                String konfirmasiPassword = binding.etKonfirmasiPassword.getText().toString();
+
+                boolean bolehRegister = true;
+                if (TextUtils.isEmpty(username)) {
+                    bolehRegister = false;
+                    binding.etUsername.setError("Username tidak boleh kosong!");
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    bolehRegister = false;
+                    binding.etPassword.setError("Password tidak boleh kosong!");
+                }
+
+                if (TextUtils.isEmpty(konfirmasiPassword)) {
+                    bolehRegister = false;
+                    binding.etKonfirmasiPassword.setError("Konfirmasi password tidak boleh kosong!");
+                }
+
+                if (!password.equals(konfirmasiPassword)) {
+                    bolehRegister = false;
+                    binding.etPassword.setError("Konfirmasi Password Tidak Sama");
+                }
+
+                if (password.length()<6) {
+                    bolehRegister = false;
+                    binding.etPassword.setError("Password minimal 6 Karakter");
+                }
+
+                if (bolehRegister) {
+                    register(username, password);
+                }
+
+            }
+        });
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void register(String username, String password) {
+        binding.progressBar.setVisibility(View.VISIBLE);
+        // memanggil API Register
+        APIService apiregister = Utility.getRetrofit().create(APIService.class);
+        Call<ValueData<User>> call = apiregister.register(username,password);
+        call.enqueue(new Callback<ValueData<User>>() {
+            @Override
+            public void onResponse(Call<ValueData<User>> call, Response<ValueData<User>> response) {
+                binding.progressBar.setVisibility(View.GONE);
+                if (response.code()==200) {
+                    int success = response.body().getSuccess();
+                    String message = response.body().getMessage();
+                    if (success == 1) {
+                        Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                        Utility.setValue(RegisterActivity.this, "xUserId", response.body().getData().getId());
+                        Utility.setValue(RegisterActivity.this, "xUsername", username);
+                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Response =" + response.code(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ValueData<User>> call, Throwable t) {
+                binding.progressBar.setVisibility(View.GONE);
+                System.out.println("Retrofit Error : " + t.getMessage());
+                Toast.makeText(RegisterActivity.this, "Retrofit Error : "+ t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
