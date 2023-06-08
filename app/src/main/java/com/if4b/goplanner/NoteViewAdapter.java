0bbf967e8972032b00afdc19ce111931ea817f5c
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-06-08 11:49:55 +0700

    Add NoteViewAdapter and WorkViewAdapter

--- app/src/main/java/com/if4b/goplanner/NoteViewAdapter.java
+++ app/src/main/java/com/if4b/goplanner/NoteViewAdapter.java
@@ -1,4 +1,63 @@
 package com.if4b.goplanner;
 
-public class NoteViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.if4b.goplanner.databinding.NoteItemBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteViewAdapter extends RecyclerView.Adapter<NoteViewAdapter.ViewHolder> {
+    private List<Note> data = new ArrayList<>();
+    private NoteViewAdapter.OnItemLongClickListener onItemLongClickListener;
+
+    public void setData(List<Note> data) {
+        this.data = data;
+    }
+
+    public void setOnItemLongClickListener(OnItemLongClickListener onItemLongClickListener) {
+        this.onItemLongClickListener = onItemLongClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(NoteItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int pos = holder.getAdapterPosition();
+        Note note = data.get(pos);
+
+        holder.noteItemBinding.tvContent.setText(note.getContent());
+        holder.noteItemBinding.tvCreatedDate.setText(note.getCreate_date());
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                onItemLongClickListener.onItemLongClick(v,pos);
+                return false;
+            }
+        });
+    }
+
+    public int getItemCount() { return data.size(); }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private NoteItemBinding noteItemBinding;
+        public ViewHolder(@NonNull NoteItemBinding itemView) {
+            super(itemView.getRoot());
+            noteItemBinding = itemView;
+        }
+    }
+
+    public interface OnItemLongClickListener {
+        void onItemLongClick(View v, int position);
+    }
 }
--- app/src/main/java/com/if4b/goplanner/WorkViewAdapter.java
+++ app/src/main/java/com/if4b/goplanner/WorkViewAdapter.java
@@ -1,4 +1,63 @@
 package com.if4b.goplanner;
 
-public class WorkViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.if4b.goplanner.databinding.WorkItemBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkViewAdapter extends RecyclerView.Adapter<WorkViewAdapter.ViewHolder> {
+    private List<Work> data = new ArrayList<>();
+    private WorkViewAdapter.onItemLongClickListener onItemLongClickListener;
+
+    public void setData(List<Work> data) {
+        this.data = data;
+    }
+
+    public void setOnItemLongClickListener(onItemLongClickListener onItemLongClickListener) {
+        this.onItemLongClickListener = onItemLongClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(WorkItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int pos = holder.getAdapterPosition();
+        Work work = data.get(pos);
+
+        holder.workItemBinding.tvContent.setText(work.getContent());
+        holder.workItemBinding.tvCreatedDate.setText(work.getCreate_date());
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                onItemLongClickListener.onItemLongClick(v, pos);
+                return false;
+            }
+        });
+    }
+
+    public int getItemCount() { return data.size(); }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private WorkItemBinding workItemBinding;
+
+        public ViewHolder(@NonNull WorkItemBinding itemView) {
+            super(itemView.getRoot());
+            workItemBinding = itemView;
+        }
+    }
+
+    public interface onItemLongClickListener {
+        void onItemLongClick(View v, int position);
+    }
 }
